95e12b9b8d600aca2a0a167e487ac038257e4287
alitolibov <alitolibov066@gmail.com>
2023-05-31 00:05:46 +0500

    add tablets adaptive

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -36,20 +36,20 @@ const Header: React.FC<HeaderProps> = () => {
         animate="visible"
         exit={{ opacity: 0, transition: { duration: 1 } }}
         variants={{ visible: { transition: { staggerChildren: 0.3 } } }}
-        className='bg px-[15px] pt-[27px] relative'>
-            <div className="flex w-full items-center justify-between max-h-[57px] h-[57px]">
-                <img src="/images/logo.png" className='w-[60px]' alt="" />
-                <nav className="flex gap-x-[10px]">
-                <p className="text-[#fff] text-[13px]">{lang.header.about}</p>
-                <p className="text-[#fff] text-[13px]">{lang.header.services}</p>
-                <p className="text-[#fff] text-[13px]">{lang.header.employees}</p>
+        className='bg px-[15px] pt-[27px] relative md:px-[25px]'>
+            <div className="flex w-full items-center justify-between h-[57px] md:h-[77px]">
+                <img src="/images/logo.png" className='w-[60px] md:w-[77px]' alt="" />
+                <nav className="flex gap-x-[10px] md:gap-x-[15px]">
+                <p className="text-[#fff] text-[13px] md:text-[15px]">{lang.header.services}</p>
+                <p className="text-[#fff] text-[13px] md:text-[15px]">{lang.header.about}</p>
+                <p className="text-[#fff] text-[13px] md:text-[15px]">{lang.header.employees}</p>
                 </nav>
                 <Language/>
             </div>
             <div className="absolute translate-y-[-50%] top-[50%] space-y-[20px]">
             <motion.h1 variants={{hidden: { opacity: 0, y: -20 }, visible}}
-            className='font-[700] text-[36px] text-white leading-[44px] w-[285px]'>{lang.header.greetings}</motion.h1>
-            <motion.div variants={itemVariants} className="flex items-center gap-x-[5px]"><p className="font-[500] text-[12px] text-white">{lang.header.scroll}</p> <img className='h-[16px]' src="/images/bottom.png" alt="" /></motion.div>
+            className='font-[700] text-[36px] text-white leading-[44px] w-[285px] md:text-[45px] md:leading-[55px] md:w-[360px]'>{lang.header.greetings}</motion.h1>
+            <motion.div variants={itemVariants} className="flex items-center gap-x-[5px]"><p className="font-[500] text-[12px] text-white md:text-[14px]">{lang.header.scroll}</p> <img className='h-[16px]' src="/images/bottom.png" alt="" /></motion.div>
             </div>
         </motion.header>
     );
