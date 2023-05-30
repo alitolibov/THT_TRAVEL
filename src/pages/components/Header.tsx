ca59c75694376d63a8641df0a21dffdc59ef9845
alitolibov <alitolibov066@gmail.com>
2023-05-30 15:01:39 +0500

    add header

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import uz from '../../../public/lang/uz';
 import ru from '../../../public/lang/ru';
 import eng from '../../../public/lang/eng';
+import Language from './Language';
 
 interface HeaderProps {
     
@@ -11,20 +12,27 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = () => {
     const {locale} = useRouter()
     let lang
-    if(locale === 'uz') {
-        lang = uz
-    } else if(locale === 'ru') {
+    switch(locale) {
+        case 'uz':
+            lang = uz
+          break
+        case 'ru': 
         lang = ru
-    } else {
-        lang = eng
-    }
+          break
+        default:
+            lang = eng
+          break
+      }
 
     return (
-        <header className='px-[24px] pt-[27px] flex w-full items-center justify-between h-[57px]'>
-            <div className="h-full w-[37px] bg-cover bg-no-repeat bg-[url('/images/logo.png')]"></div>
-            <div className="flex items-center gap-[5px]">
-                <p className="text-[#fff] text-[8px]">{lang.header.about}</p>
-                <p className="text-[#fff] text-[8px]">{lang.header.employees}</p>
+        <header className='bg px-[15px] pt-[27px]'>
+            <div className="flex w-full items-center justify-between max-h-[57px] h-[57px]">
+                <img src="/images/logo.png" className='w-[60px]' alt="" />
+                <div className="flex gap-x-[10px]">
+                <p className="text-[#fff] text-[14px]">{lang.header.about}</p>
+                <p className="text-[#fff] text-[14px]">{lang.header.employees}</p>
+                </div>
+                <Language/>
             </div>
         </header>
     );
