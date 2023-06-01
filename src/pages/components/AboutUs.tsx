07b18bc64ee5367679ab2bb0945e4f8a040d6b30
alitolibov <alitolibov066@gmail.com>
2023-06-01 15:08:00 +0500

    fixed

--- src/pages/components/AboutUs.tsx
+++ src/pages/components/AboutUs.tsx
@@ -4,6 +4,11 @@ import {motion} from 'framer-motion'
 interface AboutUsProps {
     
 }
+const visible = { opacity: 1, y: 0, transition: { duration: 0.5 } };
+    const itemVariants = {
+        hidden: { opacity: 0, y: 10 },
+        visible
+      };
 
 const animation = {
     hidden: {
@@ -19,18 +24,23 @@ const animation = {
 
 const AboutUs: React.FC<AboutUsProps> = () => {
     return (
-        <motion.section initial='hidden'
+        <motion.section 
+        initial='hidden'
         whileInView='visible'
-        viewport={{ amount: 0.2}}
+        viewport={{ amount: 0.4, once: true}}
         className='space-y-[20px] sm:flex sm:justify-between sm:items-center'>
             <div className="space-y-[40px] sm:w-[50%]">
-                <div className="relative">
-                    <p className="font-[900] text-[5rem] text-[var(--main-color-two)] opacity-[0.5] leading-[5rem]">STORY</p>
-                    <p className="font-[600] text-[1.85rem] text-[#fff] absolute bottom-[5px]">Our Story</p>
-                </div>
-                <p className="text-[#fff]">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam eveniet eos numquam placeat rem blanditiis enim? Eius, illum molestias animi aut, ullam error deleniti alias saepe vitae dolore illo ab.</p>
+                <motion.div variants={{hidden: { opacity: 0, y: -20 }, visible}} className="relative">
+                    <p  className="font-[900] text-[5rem] text-[var(--main-color-two)] opacity-[0.5] leading-[5rem]">STORY</p>
+                    <p  className="font-[600] text-[1.85rem] text-[#fff] absolute bottom-[5px]">Our Story</p>
+                </motion.div>
+                <motion.p variants={itemVariants} className="text-[#fff]">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam eveniet eos numquam placeat rem blanditiis enim? Eius, illum molestias animi aut, ullam error deleniti alias saepe vitae dolore illo ab.</motion.p>
             </div>
-            <motion.div variants={animation} className='aspect-square sm:w-[40%] bg-[url("/images/traveler.png")] bg-center bg-no-repeat bg-contain' />
+            <motion.div 
+            initial='hidden'
+            whileInView='visible'
+            viewport={{ amount: 0.4, once: true}}
+            variants={animation} className='aspect-square sm:w-[40%] bg-[url("/images/traveler.png")] bg-center bg-no-repeat bg-contain' />
         </motion.section>
     );
 };
