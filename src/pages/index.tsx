8d2151f09c9b4a47825df6d25a47fcd16df5fbcb
alitolibov <alitolibov066@gmail.com>
2023-06-06 11:14:30 +0500

    fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,7 @@ const Main: React.FC<MainProps> = () => {
         <>
         <Head>
             <title>THT VISA</title>
-            <link rel="icon" sizes="32x32" href="/images/logo.png" />
+            <link rel="icon" sizes="32x32" href="/images/logo_head.png" />
         </Head>
         <motion.div 
         initial="hidden"
