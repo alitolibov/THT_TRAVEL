c62677851f20cbca2181e890995ecc12ce5614ea
Ali-web-developer <109537573+alitolibov@users.noreply.github.com>
2023-06-20 23:17:31 +0500

    Update Services.tsx

--- src/pages/components/Services.tsx
+++ src/pages/components/Services.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 import Item from './Item';
 import { useRouter } from 'next/router';
 import uz from '../../../public/lang/uz';
