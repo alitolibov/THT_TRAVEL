45be7b3c8d78e556739c512ddb3e521361894549
alitolibov <alitolibov066@gmail.com>
2024-11-27 13:32:04 +0500

    feature/ added BurgerMenu.tsx for mobiles

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -3,28 +3,40 @@ import { motion } from 'framer-motion';
 import React from 'react';
 import Language from './Language';
 import Link from 'next/link';
-import {useTranslation} from 'next-i18next';
+import { useTranslation } from 'next-i18next';
+import BurgerMenu from '@/components/common/BurgerMenu';
 
 const Header = () => {
-    const {asPath, locale} = useRouter();
-    const {t} = useTranslation();
-
-    return (
-        <motion.header
-        initial="hidden"
-        animate="visible"
-        exit={{ opacity: 0, transition: { duration: 1 } }}
-        variants={{ visible: { transition: { staggerChildren: 0.3 } } }}
-        className='h-[20vh] flex items-center justify-between px-4 lt:px-0 container mx-auto'>
-                <Link locale={locale} href={'/'}><img src={'/images/logo.webp'} alt={'Logo'} className='w-14 md:w-20 xl:w-24'/></Link>
-                <nav className="flex gap-x-[10px] md:gap-x-4 lg:gap-x-5 xxl:gap-x-6">
-                <Link href={'/services'} locale={locale} className={`text-sm font-semibold duration-300 md:text-base lg:cursor-pointer lg:hover:text-[var(--main-color-two)] lg:text-lg xl:text-xl xxl:text-[22px] ${asPath == '/services' ? 'text-[var(--main-color-two)]' : 'text-[#fff]'}`}>{t('header.services')}</Link>
-                <Link href={'/'} locale={locale} className="text-[#fff] text-sm font-semibold duration-300 md:text-base lg:cursor-pointer lg:hover:text-[var(--main-color-two)] lg:text-lg xl:text-xl xxl:text-[22px]">{t('header.index')}</Link>
-                <Link href={'/about'} locale={locale} className={`text-[#fff] text-sm font-semibold duration-300 md:text-base lg:cursor-pointer lg:hover:text-[var(--main-color-two)] lg:text-lg xl:text-xl xxl:text-[22px] ${asPath == '/about' ? 'text-[var(--main-color-two)]' : 'text-[#fff]'}`}>{t('header.about')}</Link>
-                </nav>
-                <Language/>
-        </motion.header>
-    );
+	const { asPath, locale } = useRouter();
+	const { t } = useTranslation();
+	
+	return (
+		<>
+			<motion.header
+				initial="hidden"
+				animate="visible"
+				exit={{ opacity: 0, transition: { duration: 1 } }}
+				variants={{ visible: { transition: { staggerChildren: 0.3 } } }}
+				className="py-10 flex items-center justify-between px-4 lt:px-0 container mx-auto">
+				<Link locale={locale} href={'/'}><img src={'/images/logo.webp'} alt={'Logo'}
+				                                      className="w-14 md:w-20 xl:w-24" /></Link>
+				<nav className="hidden lg:flex gap-x-5 2xl:gap-x-6">
+					<Link href={'/services'} locale={locale}
+					      className={'font-semibold duration-300cursor-pointer hover:text-[var(--main-color-two)] lg:text-lg xl:text-xl text-white'}>{t('header.services')}</Link>
+					<Link href={'/about'} locale={locale}
+					      className={'font-semibold duration-300cursor-pointer hover:text-[var(--main-color-two)] lg:text-lg xl:text-xl text-white'}>{t('header.about')}</Link>
+					<Link href={'/tours'} locale={locale}
+					      className={'font-semibold duration-300cursor-pointer hover:text-[var(--main-color-two)] lg:text-lg xl:text-xl text-white'}>{t('header.tours')}</Link>
+				</nav>
+				<div className={'hidden lg:block'}>
+					<Language/>
+				</div>
+				<div className={'lg:hidden'}>
+					<BurgerMenu/>
+				</div>
+			</motion.header>
+		</>
+	);
 };
 
 export default Header;
--- front/src/components/common/FooterTourPage.tsx
+++ front/src/components/common/FooterTourPage.tsx
@@ -6,6 +6,7 @@ import Money from '@/components/common/Money';
 import { useRouter } from 'next/router';
 import { useTranslation } from 'next-i18next';
 import { getValue } from '@/utils';
+import OutsideClickHandler from 'react-outside-click-handler';
 
 interface FooterTourPageProps {
 	tour: ITour;
@@ -43,21 +44,23 @@ const FooterTourPage: React.FC<FooterTourPageProps> = ({ tour }) => {
 	
 	return (
 		<>
-			<motion.div initial={false} animate={isOpen ? 'open' : 'closed'} variants={modal}
-			            className={'w-full bg-[var(--main-color)] overflow-y-scroll rounded-2xl overflow-hidden border-t border-[#ffffff] bottom-[75px] fixed left-0 z-10 h-4/5 lg:hidden'}>
-				<Book tourName={tourName}/>
-			</motion.div>
-			<motion.div
-				className={`fixed bottom-0 left-0 w-full h-[75px] bg-[var(--main-color)] border-t border-[#ffffff] lg:hidden ${asPath.includes('tour/') ? 'z-50' : 'z-0'}`}>
-				<div className="flex items-center justify-between px-4 h-full">
-					<p className="text-lg font-semibold text-[var(--main-color-two)]">
-						<Money value={tour?.price} />
-					</p>
-					<button onClick={() => toggleOpen()}
-					        className="rounded-3xl text-white px-4 py-2 bg-[var(--main-color-two)] font-medium">{t('dynamicPage.bookForm.btnBook')}
-					</button>
-				</div>
-			</motion.div>
+			<OutsideClickHandler onOutsideClick={() => toggleOpen()}>
+				<motion.div initial={false} animate={isOpen ? 'open' : 'closed'} variants={modal}
+				            className={'w-full bg-[var(--main-color)] overflow-y-scroll rounded-2xl overflow-hidden border-t border-[#ffffff] bottom-[75px] fixed left-0 z-10 h-3/5 lg:hidden'}>
+					<Book tourName={tourName}/>
+				</motion.div>
+				<motion.div
+					className={`fixed bottom-0 left-0 w-full h-[75px] bg-[var(--main-color)] border-t border-[#ffffff] lg:hidden ${asPath.includes('tour/') ? 'z-50' : 'z-0'}`}>
+					<div className="flex items-center justify-between px-4 h-full">
+						<p className="text-lg font-semibold text-[var(--main-color-two)]">
+							<Money value={tour?.price} />
+						</p>
+						<button onClick={() => toggleOpen()}
+						        className="rounded-3xl text-white px-4 py-2 bg-[var(--main-color-two)] font-medium">{t('dynamicPage.bookForm.btnBook')}
+						</button>
+					</div>
+				</motion.div>
+			</OutsideClickHandler>
 		</>
 	);
 };
